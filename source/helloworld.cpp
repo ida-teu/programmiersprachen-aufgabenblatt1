572f025d208698a48c2a373ec03419c70227c7ff
Ida Teubner <ceki8317@in.uni-weimar.de>
2026-04-15 15:06:56 +0200

    Added Task 2

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
+int ggT();
+
 int main()
 {
-  std::cout << "Hallo Welt!\n";
+	std::cout << "Hallo Welt!\n" << ggT();
   return 0;
+}
+
+int ggT() {
+	int current_number = 20;
+	while (true) {
+		bool found = true;
+		for (int i = 1; i < 21; ++i) {
+			if (current_number % i != 0) {
+				found = false;
+				break;
+			}
+		}
+		if (found) {
+			return current_number;
+		}
+		else {
+			current_number += 20;
+		}
+		
+	}
 }
